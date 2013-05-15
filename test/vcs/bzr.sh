141c4ff98adf64d4b0edb0c34d30c06e5b18c77b
David Caldwell <david@porkrind.org>
2013-05-15 15:37:21 -0700

    Also test bzr

---
+++ test/vcs/bzr.sh
@@ -0,0 +1,13 @@
+bzr_init () {
+    WD=$1
+    mkdir -p "$WD"
+    (cd "$WD" && bzr init)
+
+    export VC_DIFF="bzr diff"
+    export DIFF_PREFIX=""
+}
+
+bzr_cleanup () {
+    WD=$1
+    rm -rf "$WD"
+}
