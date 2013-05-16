ecf74dd1bcacb65bdff58ca31ce78f0b3087f3f5
David Caldwell <david@porkrind.org>
2013-05-15 18:49:24 -0700

    Also test Subversion

---
+++ test/vcs/svn.sh
@@ -0,0 +1,19 @@
+svn_init () {
+    WD=$1
+    REPO=$2
+    set -e
+
+    (cd "$REPO" && svnadmin create test)
+    svn import "$WD" file://"$REPO"/test/trunk -m "Setting up trunk"
+    svn checkout file://"$REPO"/test/trunk "$WD"
+
+    export VC_DIFF="svn diff"
+    export DIFF_PREFIX=""
+}
+
+svn_cleanup () {
+    WD=$1
+    REPO=$2
+    rm -rf "$WD"
+    rm -rf "$REPO"
+}
